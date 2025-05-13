515ab72b483fae6f4cd8eee7fd62e10ca710b4fe
Haziq Amiruddin <141046553+HaziqAmiruddin@users.noreply.github.com>
2025-05-13 19:21:43 +0800

    afterthispostman

--- server.js
+++ server.js
@@ -1,20 +1,58 @@
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((request, response) => {
-    // console.log('I hear you!. thank you')
-    // console.log('header', request.headers)
-    // console.log('method', request.method)
-    // console.log('url', request.url)
+// const server = http.createServer((request, response) => {
+//     // console.log('I hear you!. thank you')
+//     // console.log('header', request.headers)
+//     // console.log('method', request.method)
+//     // console.log('url', request.url)
 
-    // response.setHeader('Content-Type','text/html');
-    // response.end('<h1>Hellooooooo</h1>');
+//     // response.setHeader('Content-Type','text/html');
+//     // response.end('<h1>Hellooooooo</h1>');
 
-    const user = {
-        name : 'John',
-        hobby : 'skating'
-    }
-    response.setHeader('Content-type', 'application/json');
-    response.end(JSON.stringify(user));
+//     const user = {
+//         name : 'John',
+//         hobby : 'skating'
+//     }
+//     response.setHeader('Content-type', 'application/json');
+//     response.end(JSON.stringify(user));
+// })
+
+// server.listen(3000);
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('HELLOOOO')
+    next();
 })
 
-server.listen(3000);
+app.get('/', (req, res) => {
+    // res.send("hellloooooo heeeloooo");
+    const user = {
+        name : 'Sally',
+        hobby : 'Soccer'
+    }
+    res.send(user);
+})
+
+app.get('/', (req, res) => {
+    res.send("getting root");
+})
+
+app.get('/profile', (req, res) => {
+    res.send("getting profile");
+})
+
+app.post('/profile', (req, res) => {
+    // res.send("hellloooooo heeeloooo");
+    const user = {
+        name : 'Sally',
+        hobby : 'Soccer'
+    }
+    res.send(user);
+})
+app.listen(3000);
